b91a829678cc11414fd1decaa171dccab7df0476
SanchitaDevkar <>
2024-08-18 07:56:18 +0000

    Updates

---
+++ _next/static/_1ZTLH4iO5x1zKJvRceDS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-3be0e833891b76dc.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
